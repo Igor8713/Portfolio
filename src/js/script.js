c562cd0ce0d3cd7e3ff9a20a5a91947514c98241
Ihor <igor.litvinenko.1996@gmail.com>
2023-12-23 23:25:45 +0200

    Created Skills section

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ document.querySelector('.hamburger').addEventListener('click', function(){
 document.querySelector('.menu__close').addEventListener('click', function(){
     document.querySelector('.menu').classList.remove('active');
 });
+
+const percs = document.querySelectorAll('.skills__progressPerc'),
+      progBars = document.querySelectorAll('.skills__progressBar span');
+
+percs.forEach((item, i) =>{
+    progBars[i].style.width = item.innerHTML;
+});
